1553824e3d47188d5b04f9f3cc35abc7b2c9ce57
Alexandre Massonneau <aam229@cornell.edu>
2014-09-26 00:30:37 -0400

    Fix file copytin issue Parse copied file to replace url based on the previous file path

--- tasks/flatten.js
+++ tasks/flatten.js
@@ -2,27 +2,29 @@ var fs = require("fs");
 
 module.exports = function(grunt){
 
-    function copyFiles(path, prefix, outputPath){
+    function parse(path, prefix, outputPath, separator, outputFiles){
         var files = fs.readdirSync(path),
             subpath;
         for(var i = 0; i < files.length; i++) {
             subpath = path + "/" + files[i];
 
             if(fs.lstatSync(subpath).isDirectory()){
-                copyFiles(subpath, prefix + "|" + files[i], outputPath);
+                parse(subpath, prefix + separator + files[i], outputPath, separator, outputFiles);
             }
             else{
                 if(files[i].indexOf(".md") === -1){
                     continue;
                 }
-                fs.createReadStream(subpath).pipe(fs.createWriteStream(filePath(outputPath, prefix, files[i])));
-                grunt.log.writeln("Copying '" + subpath + "' to '" + filePath(outputPath, prefix, files[i]) + "'");
+                outputFiles.push({
+                    inPath: subpath,
+                    outPath: filePath(outputPath, prefix, files[i], separator)
+                });
             }
         }
     }
 
-    function filePath(outputPath, prefix, name){
-        return outputPath + "/" + (prefix ? prefix + "|" : "") + name;
+    function filePath(outputPath, prefix, name, separator){
+        return outputPath + "/" + (prefix ? prefix + separator : "") + name;
     }
 
     function cleanFiles(path, prefix){
@@ -39,13 +41,35 @@ module.exports = function(grunt){
         }
     }
 
+    function write(files, src){
+        var replacements = {}, file, content, replacement;
+        for(var i = 0; i < files.length; i++){
+            file = files[i].outPath;
+            file = file.substring(file.lastIndexOf("/")+1, file.lastIndexOf("."));
+            replacement = encodeURIComponent(file);
+            file = files[i].inPath;
+            file = file.substring(src.length + 1);
+            replacements[file] = replacement;
+        }
+        grunt.log.ok(JSON.stringify(replacements));
+        for(var i = 0; i < files.length; i++){
+            content = fs.readFileSync(files[i].inPath, {encoding: "utf8"});
+            for(var url in replacements){
+                content = content.replace(new RegExp(url, "g"), replacements[url]);
+            }
+            fs.writeFileSync(files[i].outPath, content);
+        }
+    }
+
     grunt.registerMultiTask('flatten', 'Flatten a folder structure .', function() {
         // Merge task-specific and/or target-specific options with these defaults.
         var options = {
             src: this.data.src,
             dest: this.data.dest,
-            prefix: this.data.prefix
-        };
+            prefix: this.data.prefix,
+            separator: this.data.separator || "|"
+        }, files = [];
+
         if(!options.src){
             grunt.log.error("You must specify a src option");
             return false;
@@ -55,7 +79,8 @@ module.exports = function(grunt){
             return false;
         }
         cleanFiles(options.dest, options.prefix);
-        copyFiles(options.src, options.prefix, options.dest);
+        parse(options.src, options.prefix, options.dest, options.separator, files);
+        write(files, options.src);
         grunt.log.ok("The folder '" + options.src + "' was successfully flattened to '" + options.dest + "'");
     });
 };
